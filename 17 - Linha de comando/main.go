6efd38f5ac4bc9eb715fe4a63cca6fe4d655eab1
Wendell Alves <wendellmccaulay@hotmail.com>
2024-11-27 22:32:19 -0300

    Adicionar aplicação de linha de comando

---
+++ 17 - Linha de comando/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"linha-de-comando/app"
+	"log"
+	"os"
+)
+
+func main() {
+	fmt.Println("Ponto de partida")
+	aplicacao := app.Gerar()
+	erro := aplicacao.Run(os.Args)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+}
